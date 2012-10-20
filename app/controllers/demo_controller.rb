ded39cdd95aaa798a11888aea7c4e2ed4d9af743
Omar Thanawalla <omar.thanawalla@gmail.com>
2012-10-19 23:26:33 -0500

    successfully sent texts through twilio

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,38 @@
+class DemoController < ApplicationController
+
+def index
+	@myUser = User.all
+	render :json => @myUser
+end
+
+def hello
+	render("hello")
+end	
+
+def send_text_message
+    #number_to_send_to = params[:number_to_send_to]
+    
+	number_to_send_to = ["4695697074","5125601205"]
+	
+	
+	anotherNumber = "5125601205"
+    twilio_sid = "AC22d3120e5e0ec3c8c2560e647bcef3b1"
+    twilio_token = "4dd4eef0fa82a776e814d7553df64f81"
+    twilio_phone_number = "9728107682"
+
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+	for i in 0...number_to_send_to.length
+		@twilio_client.account.sms.messages.create(
+		  :from => "+1#{twilio_phone_number}",
+		  :to => number_to_send_to[i], 
+		  :body => "This is anoter message with two numbers. It gets sent to #{number_to_send_to}"
+		)
+    end
+    
+    
+    render("messageSent")
+  end
+
+
+end
