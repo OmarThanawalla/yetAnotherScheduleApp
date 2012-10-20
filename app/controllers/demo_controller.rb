a5aeb8e737b9c4965c04473cb0340f6e8277d1f5
Omar Thanawalla <omar.thanawalla@gmail.com>
2012-10-20 00:28:52 -0500

    fixed from twilio

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -38,11 +38,10 @@ def inbound
     # let's pretend that we've mapped this action to 
     # http://localhost:3000/sms in the routes.rb file
     
-    message_body = params["Body"]
-    from_number = params["From"]
+    @message_body = params["Body"]
+    @from_number = params["From"]
 
-    SMSLogger.log_text_message from_number, message_body
-    puts "inbound action calledf;alskdjf;alskdfj;alskdjf;laskdjf;alskdfj;alsdkfj;lasdkfj;alsdkfjs;aldkfj;alsdkjf;alsdkjf;lasdkfj"
+    #SMSLogger.log_text_message from_number, message_body
     #render("inbound")
   end
 
