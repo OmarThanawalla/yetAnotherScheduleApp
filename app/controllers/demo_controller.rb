84487381b9561a85a0c4c7951e75fbb82c79ff4c
Omar Thanawalla <omar.thanawalla@gmail.com>
2012-10-20 00:22:37 -0500

    inbound action created

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -34,5 +34,16 @@ def send_text_message
     render("messageSent")
   end
 
+def inbound 
+    # let's pretend that we've mapped this action to 
+    # http://localhost:3000/sms in the routes.rb file
+    
+    message_body = params["Body"]
+    from_number = params["From"]
+
+    SMSLogger.log_text_message from_number, message_body
+    puts "inbound action calledf;alskdjf;alskdfj;alskdjf;laskdjf;alskdfj;alsdkfj;lasdkfj;alsdkfjs;aldkfj;alsdkjf;alsdkjf;lasdkfj"
+    #render("inbound")
+  end
 
 end
